9e3dcc61dfb962aa08768b1c9914bcab3ef8c49f
BenMcLoughlin <benmcl@shaw.ca>
2019-06-06 10:00:18 -0700

    ready

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
-import Counter from "./Counter"
-import store from "./store"
+
 
 export default class App extends Component {
     render() {
         return (
             <div>
-               <Counter
-                   store={store}
-               />
 
             </div>
         )
